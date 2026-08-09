54ddcaa1d40bd53106c3d575479a9c58f0677061
panshi <rpriyanshi434@gmail.com>
2026-08-09 14:30:07 +0530

    Marias fih be swimmin hard

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import ("fmt"
-		"time")
+		"time"
+	"strings")
 type Fish struct {
 	Name string
 	X int
 	Y int
 	speed int
+	Art string
 }
 
 func main(){
@@ -17,14 +19,44 @@ func main(){
     X:     10,
     Y:     5,
     speed: 1,
+	Art : "𓆝",
+}
+fishPoses := []string{
+    "𓆝",
+    "𓆟",
+    "𓆞",
 }
 
-for i := 0; i < 5; i++ {
-	fish.X = fish.X + 2*fish.speed
+direction := 1
+i := 0
+maxX := 28
 
-	fmt.Printf("Fish: %s, Position: (%d, %d), Speed: %d\n", fish.Name, fish.X, fish.Y, fish.speed)
+for {
+    fish.X = fish.X + 2*fish.speed*direction
 
-	time.Sleep(500 * time.Millisecond)
+    if fish.X >= maxX {
+        direction = -1
+    }
+
+    if fish.X <= 0 {
+        direction = 1
+    }
+
+    fish.Art = fishPoses[i%len(fishPoses)]
+
+    if i > 0 {
+        fmt.Print("\033[6A")
+    }
+
+    fmt.Println("┌──────────────────────────────────┐")
+    fmt.Println("│                                  │")
+    fmt.Println("│" + strings.Repeat(" ", fish.X) + fish.Art + strings.Repeat(" ", maxX-fish.X))
+    fmt.Println("│                                  │")
+    fmt.Println("│                                  │")
+    fmt.Println("└──────────────────────────────────┘")
+
+    i++
+
+    time.Sleep(500 * time.Millisecond)
 }
 }
-
