08663b6b2274d45c01b8a82b22b41439cb15f007
panshi <rpriyanshi434@gmail.com>
2026-08-09 13:38:46 +0530

    files yey

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import ("fmt"
+		"time")
+type Fish struct {
+	Name string
+	X int
+	Y int
+	speed int
+}
+
+func main(){
+	fmt.Println("Welcome to my aquarium....Bleep...Bloop...Bloop 𓆝 ⋆｡𖦹°‧ ")
+
+	fish := Fish{
+    Name:  "Marias",
+    X:     10,
+    Y:     5,
+    speed: 1,
+}
+
+for i := 0; i < 5; i++ {
+	fish.X = fish.X + fish.speed
+
+	fmt.Printf("Fish: %s, Position: (%d, %d), Speed: %d\n", fish.Name, fish.X, fish.Y, fish.speed)
+
+	time.Sleep(5000 * time.milliSecond)
+}
+}
+
